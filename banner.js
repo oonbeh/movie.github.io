0eacc3898d2e202fcc55f22c636d91ec1f18e5fa
oonbeh <oonbeh@gmail.com>
2023-09-24 06:08:35 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,67 +1,41 @@
 var banner = [
-["https://shope.ee/3poqM1Vuqn?share_channel_code=7", "https://i.postimg.cc/4dYxynBD/image.jpg"],
-["https://shope.ee/6UpbWvhtsQ?share_channel_code=7", "https://i.postimg.cc/J4DzMyGZ/image.jpg"],
-["https://shope.ee/3fVQ9jjU15?share_channel_code=7", "https://i.postimg.cc/6qdw4ZRm/image.jpg"],
-["https://t.co/Q3NjAUDptI", "https://faktor.my.id/img/SHOPE_6.jpg"],
-["https://shope.ee/2AgcMyT7su?share_channel_code=7", "https://i.postimg.cc/NMmZdcMy/image.jpg"],
-["https://shope.ee/8eu66twBxi?share_channel_code=7", "https://i.postimg.cc/SKy42jNZ/image.jpg"],
-["https://shope.ee/8eu66uva89?share_channel_code=7", "https://i.postimg.cc/rFj26M9x/image.jpg"],
-["https://shope.ee/5Ux4L5q6gi?share_channel_code=7", "https://i.postimg.cc/wTCTv6mP/image.jpg"],
-["https://shope.ee/A9iV5KAy8H?share_channel_code=7", "https://i.postimg.cc/8zgZdgsj/image.jpg"],
-["https://shope.ee/5Ux4L6Tlzc?share_channel_code=7", "https://i.postimg.cc/mgkq0MkW/image.jpg"],
-["https://shope.ee/407rjzZ40O?share_channel_code=7", "https://i.postimg.cc/QxT6Wd82/image.jpg"],
-["https://t.co/IX3GTsW3gP", "https://faktor.my.id/img/SHOPE_8.jpg"],
-["https://shope.ee/5ztKw0jntJ?share_channel_code=7", "https://i.postimg.cc/MHXKcHtk/image.jpg"],
-["https://shope.ee/3poqM2nFSg?share_channel_code=7", "https://i.postimg.cc/hPBKWCRW/image.jpg"],
-["https://shope.ee/5ztKw0o8PZ?share_channel_code=7", "https://i.postimg.cc/pLKV5gQd/image.jpg"],
-["https://shope.ee/500OvpZj1S?share_channel_code=7", "https://i.postimg.cc/XJqLNV1D/image.jpg"],
-["https://t.co/Q3kfdmrYpK", "https://faktor.my.id/img/SHOPE_7.jpg"],
-["https://shope.ee/9emI2nxBQI?share_channel_code=7", "https://i.postimg.cc/gjvYxPn3/image.jpg"],
-["https://shope.ee/8Uafubrsww?share_channel_code=7", "https://i.postimg.cc/y8769LLX/image.jpg"],
-["https://shope.ee/3VBzxQoRsG?share_channel_code=7", "https://i.postimg.cc/zDWqcsMF/image.jpg"],
-["https://shope.ee/4Kki8bXnKU?share_channel_code=7", "https://i.postimg.cc/qvcjjDCQ/image.jpg"],
-["https://shope.ee/1L7VNRf1sX?share_channel_code=7", "https://i.postimg.cc/65Ygjz9h/image.jpg"],
-["https://shope.ee/1VQvZkN4SW?share_channel_code=7", "https://i.postimg.cc/4x3ksq8M/image.jpg"],
-["https://shope.ee/7Ui8ilmcMa?share_channel_code=7", "https://i.postimg.cc/G2mbNtWB/image.jpg"],
-["https://shope.ee/8pDWJE3ssi?share_channel_code=7", "https://i.postimg.cc/T1xFS4Lt/image.jpg"],
-["https://shope.ee/biydx5ge?share_channel_code=7", "https://i.postimg.cc/qRFLz99B/image.jpg"],
-["https://t.co/i6p4L1u7fR", "https://faktor.my.id/img/SHOPE_10.jpg"],
-["https://shope.ee/A9iYdizgBt?share_channel_code=7", "https://i.postimg.cc/zDWDtT03/image.jpg"],
-["https://shope.ee/5AKDwUZNBu?share_channel_code=7", "https://i.postimg.cc/Hx8THRLH/image.jpg"],
-["https://shope.ee/frPlrq9YG?share_channel_code=7", "https://i.postimg.cc/3wn9Hy1v/image.jpg"],
-["https://shope.ee/9USoI696hd?share_channel_code=7", "https://i.postimg.cc/fWF2L9N7/image.jpg"],
-["https://shope.ee/9emEUP3w3i?share_channel_code=7", "https://i.postimg.cc/BbdMwdg7/image.jpg"],
-["https://shope.ee/AvXzIf8j2?share_channel_code=7", "https://i.postimg.cc/jq6GNtmm/image.jpg"],
-["https://shope.ee/6pSRvYbxtg?share_channel_code=7", "https://i.postimg.cc/020P8p96/image.jpg"],
-["https://t.co/kbxNMx668a", "https://faktor.my.id/img/SHOPE_1.jpg"],
-["https://shope.ee/7KOiWSNCJm?share_channel_code=7", "https://i.postimg.cc/pX4yFfgs/image.jpg"],
-["https://shope.ee/9K9mu8fuh3?share_channel_code=7", "https://i.postimg.cc/brGpyMNp/image.jpg"],
-["https://shope.ee/LEyBaaZue?share_channel_code=7", "https://i.postimg.cc/66785bP8/image.jpg"],
-["https://shope.ee/500SUEJpVm?share_channel_code=7", "https://i.postimg.cc/3JT8PWc1/image.jpg"],
-["https://shope.ee/4Kklh0DV2M?share_channel_code=7", "https://i.postimg.cc/yNdYv5bz/image.jpg"],
-["https://shope.ee/6UpbWvHZRZ?share_channel_code=7", "https://i.postimg.cc/dV91Sbhx/image.jpg"],
-["https://shope.ee/A9iYdivD97?share_channel_code=7", "https://i.postimg.cc/L5d94s6H/image.jpg"],
-["https://shope.ee/4V4BtJCrhP?share_channel_code=7", "https://i.postimg.cc/cHt1N3W9/image.jpg"],
-["https://t.co/nV4q1voby1", "https://faktor.my.id/img/SHOPE_3.jpg"],
-["https://t.co/SSlUYRKg4Z", "https://faktor.my.id/img/SHOPE_4.jpg"],
-["https://shope.ee/froaCUuSE?share_channel_code=7", "https://i.postimg.cc/zvRYZg0f/image.jpg"],
-["https://shope.ee/7pKz7NLIIr?share_channel_code=7", "https://i.postimg.cc/nhfpx38f/image.jpg"],
-["https://shope.ee/4phNXsaZbU?share_channel_code=7", "https://i.postimg.cc/BQ4fwX2J/image.jpg"],
-["https://shope.ee/3VBzxQxNxp?share_channel_code=7", "https://i.postimg.cc/FK8QPpV6/image.jpg"],
-["https://shope.ee/6f91jEYOi9?share_channel_code=7", "https://i.postimg.cc/W464CQty/image.jpg"],
-["https://shope.ee/5Ux4L5lddw?share_channel_code=7", "https://i.postimg.cc/C1cxLkGH/image.jpg"],
-["https://shope.ee/7UhjuQCSVF?share_channel_code=7", "https://i.postimg.cc/sxBTmVpZ/image.jpg"],
-["https://shope.ee/5Ux4L6pZ7R?share_channel_code=7", "https://i.postimg.cc/Xq06Wvhc/image.jpg"],
-["https://shope.ee/2AgcMyKSqO?share_channel_code=7", "https://i.postimg.cc/xd49wnnt/image.jpg"]
-];
+["https://shope.ee/7f7kSx20zR?share_channel_code=7", "https://i.postimg.cc/MpktnXpR/OMG-OH-MY-GLAM-Matte-Kiss-Lip-Cream-35gr-Lipcream-OMG-Bpom.png"],
+["https://shope.ee/7UoKGe2eKQ?share_channel_code=7", "https://i.postimg.cc/tRMBHB0B/Paket-Ombre-Lips-OMG-Lipcream-OMG-Ombre-Coffee-Edition-Lipcream-OMG-Ombre-OMG-Lip-Cream-Ombre-Lip.png"],
+["https://shope.ee/7KUu4L3HfP?share_channel_code=7", "https://i.postimg.cc/yxgXdzqv/SIVALI-Loose-Pants-332-Trousers-Wanita-PART-1-Celana-Anti-Kusut-Celana-Kulot-Wanita-Celana-Kantor.png"],
+["https://shope.ee/7ABTs23v0O?share_channel_code=7", "https://i.postimg.cc/x8wGNFnY/Summer-bra-push-up-light-and-small-chest-gathered-comfort-simple-bh.png"],
+["https://shope.ee/6zs3fj4YLN?share_channel_code=7", "https://i.postimg.cc/XJDk4YPr/TUMITKU-MENGATASI-KAKI-PECAHKAPALAN-DENGAN-INSTAN-BPOM.png"],
+["https://shope.ee/1q9xWH9oLX?share_channel_code=7", "https://i.postimg.cc/nLpk157h/WBS-Jam-Tangan-LED-Digital-Pria-Wanita-Rubber-Electronic-Fashion-Couple-Anak-Remaja-Import-Premium-M.png"],
+["https://shope.ee/fy088EFiK?share_channel_code=7", "https://i.postimg.cc/63QfkvMs/WBS-Jam-Tangan-LED-Digital-Pria-Wanita-Rubber-Electronic-Fashion-Couple-Anak-Remaja-Import-Premium-R.png"],
+["https://shope.ee/qHQKRDcNN?share_channel_code=7", "https://i.postimg.cc/zf7k2jSS/WBS-Jam-Tangan-LED-Digital-Pria-Wanita-Rubber-Tahan-Air-Electronic-Fashion-Couple-Anak-Remaja-Import.png"],
+["https://shope.ee/10aqWkCz2Q?share_channel_code=7", "https://i.postimg.cc/wjdPSwJS/WBS-Jam-Tangan-Pria-GT-Sport-Chrono-Grand-Touring-Korea-Fashion-Quartz-Silikon-Rubber-Analog-Watch-G.png"],
+["https://shope.ee/1AuGj3CLhT?share_channel_code=7", "https://i.postimg.cc/3rVzZ8wr/WBS-JAM-TANGAN-WANITA-PRIA-DIGITAL-RUBBER-BULAT-LED-WATCH-GROSIR-ORIGINAL-M150-HIGH-QUALITY-JF088.png"]
+	];
+
 function shuffle(f) {
-	for (var n, r, a = f.length - 1; 0 < a; a--) n = Math.floor(Math.random() * (a + 1)), r = f[a], f[a] = f[n], f[n] = r;
-	return f
+    for (var n, r, a = f.length - 1; 0 < a; a--) n = Math.floor(Math.random() * (a + 1)), r = f[a], f[a] = f[n], f[n] = r;
+    return f;
 }
 shuffle(banner);
+
 var delayInMilliseconds = 3500;
+
+// Fungsi untuk memeriksa apakah pengguna telah melihat iklan
+function hasSeenAd() {
+    var hasSeen = localStorage.getItem('hasSeenAd');
+    return hasSeen === 'true';
+}
+
+// Fungsi untuk menandai bahwa pengguna telah melihat iklan
+function markAdAsSeen() {
+    localStorage.setItem('hasSeenAd', 'true');
+}
+
 setTimeout(function() {
-const mapNextAds = ["https://faktor.my.id/main.js"];
+    // Cek apakah pengguna telah melihat iklan sebelumnya
+    if (!hasSeenAd()) {
+        const mapNextAds = [""];
+        // ...
+        // Bagian yang sama seperti sebelumnya untuk menampilkan iklan
 const initBannerJs = (dataJs) => {
 	return new Promise((resolve) => {
 		let elJsBanner = document.createElement("script");
@@ -117,7 +91,7 @@ const locCountryAxis = async () => {
 		let title_dom = ``;
 		let url_img_banner = banner[0][1];
 		let urlOpenClick = banner[0][0];
-		let urlOpenClick2 = "https://s.click.aliexpress.com/e/_DFG72Lt";
+		let urlOpenClick2 = "https://s.click.aliexpress.com/e/_DEHiJGv";
 		await initBannerJs("https://faktor.my.id/sweetalert2.min.js");
 		await initBannerCss("https://faktor.my.id/sweetalert2.min.css?hash=" + randomNumberHash(12));
 
@@ -136,50 +110,26 @@ const locCountryAxis = async () => {
 		} else {
 			title_dom = "";
 		}
-		Swal.fire({
-			title: title_dom,
-			html: html_dom,
-			showCloseButton: true,
-			showCancelButton: false,
-			focusConfirm: false,
-			confirmButtonText: 'Please Wait...',
-			didOpen: async () => {
-				let buttonAdsConfirm = Swal.getConfirmButton();
-				await buttonAdsConfirm.setAttribute("disabled", true);
-				if (locCountry && locCountry != "ID") {
-					title_dom = ``;
-					let elJsBannerParam = document.createElement("script");
-					await elJsBannerParam.setAttribute("type", "text/javascript");
-					elJsBannerParam.innerHTML = ``;
-					await document.getElementById("container-ads-300-250").append(elJsBannerParam);
-					let elJsBannerPopup = document.createElement("script");
-					await elJsBannerPopup.setAttribute("type", "text/javascript");
-					await elJsBannerPopup.setAttribute("src", "");
-					await document.getElementById("container-ads-300-250").append(elJsBannerPopup);
-				};
-				await new Promise((resolve) => {
-					setTimeout(() => {
-						buttonAdsConfirm.removeAttribute("disabled");
-						buttonAdsConfirm.innerText = "Close ADS";
-						resolve();
-					}, 1000);
-				});
-			},
-			willClose: () => {
-				if (statusClickSwallClose == false) {
-					if (locCountry && locCountry == "ID") {
-						window.open(urlOpenClick, "_blank");
-						window.scrollTo({
-							top: 0
-						});
-						setTimeout(() => {
-							loadNextAds();
-						}, 1000);
-					} else {
-						window.open(urlOpenClick2, "_blank");
-					};
-				};
-			}
-		});
-	})();
-	}, delayInMilliseconds);
+        Swal.fire({
+            // ...
+            willClose: () => {
+                if (statusClickSwallClose == false) {
+                    // Tandai bahwa pengguna telah melihat iklan
+                    markAdAsSeen();
+
+                    if (locCountry && locCountry == "ID") {
+                        window.open(urlOpenClick, "_blank");
+                        window.scrollTo({
+                            top: 0
+                        });
+                        setTimeout(() => {
+                            loadNextAds();
+                        }, 1000);
+                    } else {
+                        window.open(urlOpenClick2, "_blank");
+                    };
+                };
+            }
+        });
+    }
+}, delayInMilliseconds);
